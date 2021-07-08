c69b868606d90dcdac352b75bc15fa851aa9e272
Conor Meldrum <conor.j.meldrum@gmail.com>
2021-07-07 21:39:58 -0400

    adds encounter retrieval functions

--- server/db-api.js
+++ server/db-api.js
@@ -1,15 +1,41 @@
 import {database} from "./database.js";
 
 export const encounterRepository = {
-  loadEncounter: async () => {
-    const result = await database.select().from('encounters')
+  newEncounter: async (newEncounterData) => {
+    await database.insert(newEncounterData).into('encounters');
+  },
+
+  loadActiveEncounter: async () => {
+    const result = await database.select().from('encounters').where('active', true)
     return result
-  }
+  },
+
+  loadEncounterByName: async (encounterName) => {
+    const result = await database.select().from('encounters').where('name', encounterName)
+    return result
+  },
+
+  //updateEncounter
+
+  //deleteEncounter
 }
 
 export const entityRepository = {
-  loadEntities: async () => {
-    const result = await database.select().from('entities')
+  newEntity: async (newEntityData) => {
+    await database.insert(newEntityData).into('entities');
+  },
+
+  loadEntitiesByType: async (entityType) => {
+    //hero, enemy, npc, event
+    const result = await database.select().from('entities').where('entityType', entityType)
     return result
+  },
+
+  loadEntityById: async (id) => {
+    const result = await database.select().from('entities').where('id', id)
   }
+
+  //updateEntity
+
+  //deleteEntity
 }
